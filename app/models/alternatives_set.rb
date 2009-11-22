7c4328516dc106a4b241efa7ae0223c26f56a342
Tom Calloway <tomdeme@gmail.com>
2009-11-22 15:57:39 -0600

    fixed the creation of alternatives associated with a new set

--- app/models/alternatives_set.rb
+++ app/models/alternatives_set.rb
@@ -4,7 +4,16 @@ class AlternativesSet < ActiveRecord::Base
 
   def new_alternative_attributes=(alternative_attributes)
     alternative_attributes.each do |attributes|
-      alternatives.build(attributes)
+      if attributes[:id].blank?
+        alternatives.build(attributes)
+      else # precondition: alternatives is not nil  && attributes[:id] is not blank
+           #               attributes[:id] is an integer encoded as a character string
+        #alternative = alternatives.find {|t| t.id == attributes[:id].to_i}
+        alternative = Alternative.find(attributes[:id].to_i)
+        alternatives << alternative
+        #alternative.id = attributes[:id]
+        attributes.delete(:id)
+      end
     end
   end
 
