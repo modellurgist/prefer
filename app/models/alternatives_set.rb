937a3e0a49af2e63bab55539d9ca183b8783cc3d
Tom Calloway <tomdeme@gmail.com>
2009-11-22 15:58:06 -0600

    removed commented code

--- app/models/alternatives_set.rb
+++ app/models/alternatives_set.rb
@@ -6,12 +6,9 @@ class AlternativesSet < ActiveRecord::Base
     alternative_attributes.each do |attributes|
       if attributes[:id].blank?
         alternatives.build(attributes)
-      else # precondition: alternatives is not nil  && attributes[:id] is not blank
-           #               attributes[:id] is an integer encoded as a character string
-        #alternative = alternatives.find {|t| t.id == attributes[:id].to_i}
+      else 
         alternative = Alternative.find(attributes[:id].to_i)
         alternatives << alternative
-        #alternative.id = attributes[:id]
         attributes.delete(:id)
       end
     end
