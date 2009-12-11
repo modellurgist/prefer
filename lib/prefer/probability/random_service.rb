284acff4d0fae6c9687b2a21f9243a8fc35e7e0a
Tom Calloway <tomdeme@gmail.com>
2009-12-11 14:23:19 -0600

    small refactor.

--- lib/prefer/probability/random_service.rb
+++ lib/prefer/probability/random_service.rb
@@ -10,11 +10,21 @@ class RandomService
   # assumption for consistency with Kernal.rand:  
   #   delegate should return a random floating point number between 0 and 1 when passed 0 or nil (which should default to 0)
   def select_integer_from_zero_to_one_less_than(highest_integer)
-    rand(highest_integer)
+    select_pseudorandom_integer_from_zero_to_one_less_than(highest_integer)
   end
 
   # private
 
+  def select_pseudorandom_integer_from_zero_to_one_less_than(highest_integer)
+    rand(highest_integer)
+  end
 
+  def select_true_random_integer_from_zero_to_one_less_than(highest_integer)
+    if (highest_integer == 0) # should return a decimal >= 0 and < 1.0
+      return (select_true_random_integer_from_zero_to_one_less_than(1, 0, 9999) / 10000)
+    else
+      return @true_generator.randnum(1, 0, (highest_integer-1))
+    end
+  end
 
 end
