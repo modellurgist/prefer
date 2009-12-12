6963c819b48b51d9aa2083b979209fda44365874
Tom Calloway <tomdeme@gmail.com>
2009-12-11 22:12:08 -0600

    distribution may be specified by symbol in CitizenFactory

--- lib/prefer/citizen/citizen_factory.rb
+++ lib/prefer/citizen/citizen_factory.rb
@@ -18,26 +18,22 @@ class CitizenFactory
 
   # private
 
-  def build #DEPRECATED
-    Citizen.new(build_profile)
+  def build_all_profiles(population_size, distribution_symbol)
+    # TODO: don't use collect, just ask preference factory for collection with given distribution
+    @preference_factory.send("build_all_profiles_from_#{distribution_symbol.to_s}_distribution".to_sym, population_size)
   end
 
   def build_with_profile(profile)
     Citizen.new(profile)
   end
 
-  def build_all_profiles(population_size, distribution_symbol)
-    # TODO: don't use collect, just ask preference factory for collection with given distribution
-    profiles = population_size.times.collect { build_profile_from_approximate_uniform_distribution }
+  def build #DEPRECATED
+    Citizen.new(build_profile)
   end
 
   def build_profile # DEPRECATED
-    build_profile_from_approximate_uniform_distribution
-  end
-
-  def build_profile_from_approximate_uniform_distribution
     @preference_factory.uniformly_random_permutation
   end
-  
+
 
 end
--- lib/prefer/preference/preference_factory.rb
+++ lib/prefer/preference/preference_factory.rb
@@ -8,11 +8,15 @@ class PreferenceFactory
     @random = RandomService.new
   end
 
-  def uniformly_random_permutation
+  def build_all_profiles_from_approximate_uniform_distribution(population_size)
+    population_size.times.collect {uniformly_random_permutation}
+  end
+
+  # private
+
+  def uniformly_random_permutation # DEPRECATED as public, but use as private
     @alternatives.sort_by {|alternative| @random.select_integer_from_zero_to_one_less_than(0)}
   end
-  
-  # private
 
 
 end
